7c3bab07d5c133892e1e9a214ce34ff71af4b8ce
Nabil Ervatra <67039871+nabilervatra@users.noreply.github.com>
2022-11-12 17:54:32 +0000

    Chore: Adding get by id and get average

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -5,13 +5,32 @@ import { PostedReview } from './model'
 class GuestBook {
   reviews: PostedReview[] = [];
   @call({})
-  add_review({  text, rate }: { text: string , rate: number }) {
-    const review = new PostedReview({ text, rate });
+  add_review({  id, text, rate }: { id: string, text: string , rate: number }) {
+    const review = new PostedReview({ id, text, rate });
     this.reviews.push(review);
   }
 
   @view({})
-  get_reviews({ from_index = 0, limit = 10 }: { from_index: number, limit: number }): PostedReview[] {
-    return this.reviews.slice(from_index, from_index + limit);
+  get_reviews({ from_index = 0, limit = this.reviews.length }: { from_index: number, limit: number }): PostedReview[] {
+    return this.reviews.slice();
+  }
+  get_reviews_by_id({id}: {id: string}): PostedReview[] {
+    let filtered = [];
+    for(let i =0;i<this.reviews.length;i++){
+      if(this.reviews[i].id == id) filtered.push(this.reviews[i]);
+    }
+    return filtered.slice();
+  }
+  get_average_rating({id}: {id:string}): number {
+    let sum =0;
+    let cnt =0;
+    for(let i =0;i<this.reviews.length;i++){
+      if(this.reviews[i].id == id){
+        sum = sum+this.reviews[i].rate;
+        cnt++;
+      } 
+    }
+    if(cnt==0)return 0;
+    else return sum/cnt
   }
 }
